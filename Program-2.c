566b5488e7074ef449679d27087a6f94bb9cb87b
rajendragurukar <20ec0027@vvce.ac.in>
2021-03-09 10:04:49 +0000

    ok

--- Program-2.c
+++ Program-2.c
@@ -1,12 +1,12 @@
 //INPUT: 6 4
 //OUTPUT: 6 + 4 = 10
-#include <stdio.h>
+#include<stdio.h>
 int main() {    
 
     int number1, number2,sum ; // correct the code
     
     printf("Enter two integers ");
-    scanf("%d%d", &number1, &number2);
+    scanf("%d%d", &number1,&number2);
 
     // calculating sum
     sum =number1+number2;      // insert the missing code
