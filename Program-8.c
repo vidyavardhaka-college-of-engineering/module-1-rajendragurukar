f392589ef6248dc436971825b1c6d8c63ece52cd
rajendragurukar <20ec0027@vvce.ac.in>
2021-03-09 18:15:45 +0000

    ok

--- Program-8.c
+++ Program-8.c
@@ -5,10 +5,11 @@ int main()
 {
     int x,y;
     printf(" Enter the value of x: ");
-    //scan"y = %d  ",y=++x)
-    printf("y = %d  ",y=x++)
-    printf("y = %d  ",y=--x)
-    printf("y = %d  ",y=x--) // correct the code 
+    scanf("%d",&x);
+    printf("y = %d  ",y=++x);
+    printf("y = %d  ",y=x++);
+    printf("y = %d  ",y=--x);
+    printf("y = %d  ",y=x--); // correct the code 
     return 0;
 }
 
