df6734a900d84ec82706bcee12b016f924780a4d
rajendragurukar <20ec0027@vvce.ac.in>
2021-03-09 17:40:28 +0000

    ok

--- Program-5.c
+++ Program-5.c
@@ -3,12 +3,12 @@
 #include<stdio.h>
  int main ()
 { 
-     // insert the missing code
+   int i,j;  // insert the missing code
 printf("Enter the values of i and j");
 scanf("%d%d",&i,&j);
      i = i + 1;
       j = j + i + 1 ; // correct the code
-      printf("%d %d",i + 1, j - 1);
-      printf("%d %d", i, j);
+      printf("%d %d  ",i + 1, j - 1);
+      printf("%d %d  ", i, j);
 return 0;
 }
